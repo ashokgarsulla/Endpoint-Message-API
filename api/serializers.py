3d6215ca122421c02c2d290018f56312b5690b84
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-06 21:03:02 +0530

    corrected in Userserilizer

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
  class Meta:
   model = User
-  fields = ['id', 'name', 'email']
+  fields = ['id', 'username', 'email']
 
 class MessageSerializer(serializers.ModelSerializer):
  created_by = UserSerializer(read_only =True, many = False)
